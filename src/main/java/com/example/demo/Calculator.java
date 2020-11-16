06184bb645297eae03d7266021fd9de5817c39c5
Vakerin Eugene <utyfdfrthby08@gmail.com>
2020-11-16 17:01:26 +0200

    adding java doc + rounding of result

--- src/main/java/com/example/demo/Calculator.java
+++ src/main/java/com/example/demo/Calculator.java
@@ -13,8 +13,8 @@ public class Calculator {
                 double num2 = getDouble();
                 String operation = getOperation();
                 double result = calc(num1, num2, operation);
-                //double result2 = calc2(num1, num2, Operations.ADDITION);
-                System.out.println("Результат операции: " + result);
+                String result2 = String.format("%.3f", result);
+                System.out.println("Результат операции: " + result2);
                 System.out.println("Хотите еще выполнить операции ?");
                 System.out.println("Да - 1");
                 System.out.println("Нет - 2");
@@ -30,6 +30,11 @@ public class Calculator {
 
     }
 
+    /**
+     * Getting value from console
+     *
+     * @return number from console
+     */
     public static double getDouble() {
         System.out.println("Введите число:");
         double num;
@@ -43,6 +48,11 @@ public class Calculator {
         return num;
     }
 
+    /**
+     * Getting operation from console
+     *
+     * @return operation
+     */
     public static String getOperation() {
         System.out.println("Введите операцию:");
         String operation;
@@ -56,6 +66,14 @@ public class Calculator {
         return operation;
     }
 
+    /**
+     * Final calculation
+     *
+     * @param num1      first number from console
+     * @param num2      second number from console
+     * @param operation operation from console
+     * @return result of calculation
+     */
     public static double calc(double num1, double num2, String operation) {
         double result;
         switch (operation) {
@@ -71,6 +89,12 @@ public class Calculator {
         return result;
     }
 
+    /**
+     * @param num1
+     * @param num2
+     * @param operations
+     * @return
+     */
     public static double calc2(double num1, double num2, Operations operations) {
         double result;
         switch (operations) {
